b17344c070c63568797696eb86da0ab6b39d95c4
jsr029 <jsr059@gmail.com>
2022-03-06 18:32:09 +0100

    Update

--- Bill-app/Billed-app-FR-Front/src/containers/Bills.js
+++ Bill-app/Billed-app-FR-Front/src/containers/Bills.js
@@ -12,12 +12,12 @@ export default class {
     if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill)
     const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`)
     if (iconEye) iconEye.forEach(icon => {
-      icon.addEventListener('click', () => this.handleClickIconEye(icon))
+      icon.addEventListener('click', (e) => this.handleClickIconEye(icon))
     })
     new Logout({ document, localStorage, onNavigate })
   }
 
-  handleClickNewBill = () => {
+  handleClickNewBill = e => {
     this.onNavigate(ROUTES_PATH['NewBill'])
   }
 
