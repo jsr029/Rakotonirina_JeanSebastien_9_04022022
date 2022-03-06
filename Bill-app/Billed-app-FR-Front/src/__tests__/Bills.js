212c42a76e1ad2a81bcbca66ebfb01811e02c05b
jsr029 <jsr059@gmail.com>
2022-03-06 18:04:32 +0100

    Update

--- Bill-app/Billed-app-FR-Front/src/__tests__/Bills.js
+++ Bill-app/Billed-app-FR-Front/src/__tests__/Bills.js
@@ -7,7 +7,7 @@ import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
-
+import mockStore from "../__mocks__/store.js";
 import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
@@ -40,3 +40,33 @@ describe("Given I am connected as an employee", () => {
     })
   })
 })
+// test d'intégration GET
+  // test d'intégration GET
+  describe("Given I am a user connected as Employee", () => {
+    describe("When I navigate to Dashboard", () => {
+      test("fetches bills from mock API GET", async () => {
+        const getSpy = jest.spyOn(mockStore, "get")
+        const bills = await mockStore.get()
+        expect(getSpy).toHaveBeenCalledTimes(1)
+        expect(bills.data.length).toBe(4)
+      })
+      test("fetches bills from an API and fails with 404 message error", async () => {
+        mockStore.get.mockImplementationOnce(() =>
+          Promise.reject(new Error("Erreur 404"))
+        )
+        const html = BillsUI({ error: "Erreur 404" })
+        document.body.innerHTML = html
+        const message = await screen.getByText(/Erreur 404/)
+        expect(message).toBeTruthy()
+      })
+      test("fetches messages from an API and fails with 500 message error", async () => {
+        mockStore.get.mockImplementationOnce(() =>
+          Promise.reject(new Error("Erreur 500"))
+        )
+        const html = BillsUI({ error: "Erreur 500" })
+        document.body.innerHTML = html
+        const message = await screen.getByText(/Erreur 500/)
+        expect(message).toBeTruthy()
+      })
+    })
+  })
