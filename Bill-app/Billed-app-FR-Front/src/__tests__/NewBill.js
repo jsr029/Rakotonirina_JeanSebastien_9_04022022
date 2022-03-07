6e6d0843b5a06a252e51dd5a798a25b4730044ce
jsr029 <jsr059@gmail.com>
2022-03-07 09:17:35 +0100

    Update

--- Bill-app/Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Bill-app/Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -9,10 +9,13 @@ import NewBill from "../containers/NewBill.js"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    test("Then the newBill page should be rendered", () => {
+      document.body.innerHTML = NewBillUI()
+      expect(screen.getAllByText("Envoyer une note de frais")).toBeTruthy()
     })
-  })
+    test("Then a form with nine fields should be rendered", () => {
+      document.body.innerHTML = NewBillUI()
+      const form = document.querySelector("form")
+      expect(form.length).toEqual(9)
+    })
 })
